27587b61b41209fa729f88ff5ce3d813efc85937
JaimeWX <1261360793@qq.com>
2023-06-14 17:45:38 +0800

    update

--- 单链表/203_移除链表元素.cpp
+++ 单链表/203_移除链表元素.cpp
@@ -1,9 +1,9 @@
-    struct ListNode
-    {
-        int val;
-        ListNode *next;
-        ListNode(int x): val(x), next(nullptr){}
-    };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(nullptr){}
+};
 
 class Solution {
 public:
--- 双指针/反转链表_206.cpp
+++ 双指针/反转链表_206.cpp
@@ -1,3 +1,10 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(nullptr){}
+};
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) 
