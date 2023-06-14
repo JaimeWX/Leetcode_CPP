1bc4524452f6367d9f8c596c53eaa302ec914f8f
JaimeWX <1261360793@qq.com>
2023-06-14 17:24:59 +0800

    双指针

---
+++ 双指针/反转链表_206.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+
+        ListNode* tmp;
+        while(cur != nullptr)
+        {
+            tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+
+        return pre;
+    }
+};
