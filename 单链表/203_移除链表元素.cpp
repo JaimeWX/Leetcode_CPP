eef0a4bdc4f1736f0c7d1bff28c4ce9da710b082
JaimeWX <1261360793@qq.com>
2023-06-14 17:03:01 +0800

    单链表

--- 单链表/203_移除链表元素.cpp
+++ 单链表/203_移除链表元素.cpp
@@ -1,53 +1,28 @@
-/*
- * @lc app=leetcode.cn id=203 lang=cpp
- *
- * [203] 移除链表元素
- */
-
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-// struct ListNode
-// {
-//     int val;
-//     ListNode *next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode *next) : val(x), next(next) {}
-// };
-
+    struct ListNode
+    {
+        int val;
+        ListNode *next;
+        ListNode(int x): val(x), next(nullptr){}
+    };
 
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) 
-    {
-        ListNode *dummyHead = new ListNode();
+    {   
+        ListNode* dummyHead = new ListNode(0);
         dummyHead->next = head;
-        ListNode *cur = dummyHead;
+        ListNode* cur = dummyHead;
         while(cur->next != nullptr)
         {
             if(cur->next->val == val)
-            {
-                ListNode *tmp = cur->next;
+            {   
+                ListNode* tmp = cur->next;
                 cur->next = cur->next->next;
                 delete tmp;
-            }
+            } 
             else
                 cur = cur->next;
         }
-        head = dummyHead->next;
-        delete dummyHead;
-        return head;
+        return dummyHead->next;
     }
 };
-// @lc code=end
-
