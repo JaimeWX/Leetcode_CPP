cdc4f64f99441daebd7283ecb518c484d2260f75
JaimeWX <1261360793@qq.com>
2023-06-15 15:35:29 +0800

    单链表

--- 单链表/24_两两交换链表中的节点.cpp
+++ 单链表/24_两两交换链表中的节点.cpp
@@ -1,56 +1,29 @@
-/*
- * @lc app=leetcode.cn id=24 lang=cpp
- *
- * [24] 两两交换链表中的节点
- */
-
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-// struct ListNode
-// {
-//     int val;
-//     ListNode *next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode *next) : val(x), next(next) {}
-// };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(nullptr){}
+};
 
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) 
-    {   
-        if(head == nullptr || head->next == nullptr) return head;
-        ListNode *dummtHead = new ListNode(0);
-        ListNode *cur = dummtHead;
-        cur->next = head;
-        while(cur->next != nullptr)
-        {   
-            if(cur->next->next != nullptr)
-            {
-            ListNode *s1 = cur->next;
-            ListNode *s2 = cur->next->next;
-            ListNode *tmp = s2->next;
+    {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while(cur->next != nullptr && cur->next->next != nullptr)
+        {
+            ListNode* tmp1 = cur->next;
+            ListNode* tmp2 = cur->next->next->next;
 
-            cur->next = s2;
-            cur->next->next = s1;
+            cur->next = cur->next->next;
+            cur->next->next = tmp1;
+            cur->next->next->next = tmp2;
+
             cur = cur->next->next;
-            cur->next = tmp;
-            }
-            else
-                cur = cur->next;
         }
-        return dummtHead->next;
+        return dummyHead->next;
     }
 };
-// @lc code=end
 
--- 单链表/24_递归.cpp
+++
@@ -1,7 +0,0 @@
-ListNode* swapPairs(ListNode* head)
-{
-    ListNode *newHead = head->next;
-    head->next = swapPairs(newHead->next);
-    newHead->next = head;
-    return newHead;
-}
